86e1bc78c050141b663432e7377ac4455c7d6600
Tommy <86322453+TechyTommy3@users.noreply.github.com>
2022-01-13 18:06:57 -0600

    Create mystery.js

--- swerve/mystery.js
+++ swerve/mystery.js
@@ -1,1 +1,1 @@
-//Mystery Swerve
+/*Define the */function getRandomArbitrary(min, max) { return Math.random() * (max - min) + min; }; /*Set MAX_BLOCKS*/ MAX_BLOCKS = Math.floor(getRandomArbitrary(1, 23));
